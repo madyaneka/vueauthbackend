3247bc145c04886c42d9a9880ba8559c965e8180
Madyan Eka <madyan.eka95@gmail.com>
2018-11-03 13:35:17 +0700

    using all installed dependecies

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+// creating an express instance
+const app = express()
+const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+
+// getting the local authentication type
+const localStrategy = require('passport-local').Strategy
+
+app.use(bodyParser.json())
+
+app.use(cookieSession({
+  name: 'mysession',
+  keys: ['vueauthrandomkey'],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+app.use(passport.initialize())
+
+app.use(passport.session())
