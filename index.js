6c3befebe597a49395ad6f9dfba4022cf23848a0
Madyan Eka <madyan.eka95@gmail.com>
2018-11-03 14:15:36 +0700

    configure passport so it knows how to log us in

--- index.js
+++ index.js
@@ -77,3 +77,24 @@ app.get('/api/user', authMiddleware, (req, res) => {
 
   res.send({ user: user })
 })
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+
+    (username, password, done) => {
+      let user = users.find((user) => {
+        return user.email === username && user.password === password
+      })
+
+      if (user) {
+        done(null, user)
+      } else {
+        done(null, false, { message: 'Incorrect username or password'})
+      }
+    }
+  )
+)
