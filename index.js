33b5b46944c724b2dce1e2e9516b3fd4e5619657
Madyan Eka <madyan.eka95@gmail.com>
2018-11-03 14:07:21 +0700

    create login api

--- index.js
+++ index.js
@@ -35,3 +35,19 @@ const users = [
     password: 'password2'
   }
 ]
+
+app.post('/api/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err)
+    }
+
+    if (!user) {
+      return res.status(400).send([user, 'Cannot log in', info])
+    }
+
+    req.login(user, err => {
+      res.send('Logged in')
+    });
+  })(req, res, next)
+})
